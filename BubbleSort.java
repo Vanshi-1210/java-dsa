92d75f3dd0157d5104425269a9592cca0f7eb31f
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-03 16:41:56 +0530

    Added bubble sort program

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+// Program to sort array elements using bubble sort
+public class BubbleSort {
+   public static void main(String[] args) {
+    int arr[]={3,5,6,2,1};
+    for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+               int temp=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=temp;
+            }
+        }
+    }
+    printArray(arr);
+   } 
+
+public static void printArray(int arr[]){
+    for(int i=0;i<arr.length;i++){
+     System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+}
+}
